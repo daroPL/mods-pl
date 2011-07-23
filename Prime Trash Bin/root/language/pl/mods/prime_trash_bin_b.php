9b905e95d0d9c79ca6524ba3d289d3f035581d61
Dariusz Maciejewski <dariuszwm@gmail.com>
2011-07-23 00:30:39 -0700

    Edited Prime Trash Bin/root/language/pl/mods/prime_trash_bin_b.php via GitHub

--- Prime Trash Bin/root/language/pl/mods/prime_trash_bin_b.php
+++ Prime Trash Bin/root/language/pl/mods/prime_trash_bin_b.php
@@ -5,7 +5,7 @@
 *
 * @package language
 * @version $Id: prime_trash_bin_b.php,v 1.0.5 2008/08/23 17:01:00 primehalo Exp $
-* @copyright (c) 2007 Ken F. Innes IV
+* @copyright (c) 2011 daroPL dariuszwm@gmail.com http://www.phpbb.pl
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
 */
@@ -69,19 +69,19 @@ $lang = array_merge($lang, array(
 
 // Administration
 $lang = array_merge($lang, array(
-	'PRIME_FAKE_DELETE'					=> 'Usunięto temat',
-	'PRIME_FAKE_DELETE_EXPLAIN'			=> 'Określa sposób w jaki tematy będą usuwane.',
-	'PRIME_FAKE_DELETE_DISABLE'			=> 'Trwale usuwa temat',
-	'PRIME_FAKE_DELETE_ENABLE'			=> 'Zachowuje temat i zaznacza jako usunięty',
-	'PRIME_FAKE_DELETE_AUTO_TRASH'		=> 'Przenosi temat do kosza',
-	'PRIME_FAKE_DELETE_SHADOW_ON'		=> 'Przenosi temat do kosza pozostawiając cień',
-	'PRIME_FAKE_DELETE_SHADOW_OFF'		=> 'Przenosi temat do kosza niepozostawiając cienia',
+	'PRIME_FAKE_DELETE'					=> 'Usunięty temat',
+	'PRIME_FAKE_DELETE_EXPLAIN'			=> 'Określ sposób w jaki tematy będą usuwane.',
+	'PRIME_FAKE_DELETE_DISABLE'			=> 'Zostaje trwale usunięty',
+	'PRIME_FAKE_DELETE_ENABLE'			=> 'Zostaje zachowany, ale oznaczony jako usunięty',
+	'PRIME_FAKE_DELETE_AUTO_TRASH'		=> 'Trafia do kosza',
+	'PRIME_FAKE_DELETE_SHADOW_ON'		=> 'Trafia do kosza pozostawiając cień',
+	'PRIME_FAKE_DELETE_SHADOW_OFF'		=> 'Trafia do kosza nie pozostawiając cienia',
 
 	'PRIME_TRASH_FORUM'					=> 'Forum kosza',
-	'PRIME_TRASH_FORUM_EXPLAIN'			=> 'Jeśli jest zaznaczone, usuwanie temat będą przenoszone do tego forum. Usuwanie tematów z kosza usunie je na stałe.',
+	'PRIME_TRASH_FORUM_EXPLAIN'			=> 'Zaznacz, aby usuwane tematy były przenoszone do tego forum. Usuwanie tematów z kosza usunie je trwale.',
 	'PRIME_TRASH_FORUM_DISABLE'			=> 'Nie używaj kosza',
 	'PRIME_TRASH_FORUM_DIVIDER'			=> '---------------------------',
-	'PRIME_NO_TRASH_FORUM_ERROR'		=> 'Musisz ustawić forum kosza wybierjąc "%s" opcje',
+	'PRIME_NO_TRASH_FORUM_ERROR'		=> 'Musisz ustawić forum kosza wybierając opcję "%s"',
 
 	'PRIME_FOREVER_WHEN_DELETE_USER'	=> 'Trwałe usuń posty',
 ));
@@ -98,14 +98,14 @@ $lang = array_merge($lang, array(
 	'PRIME_DELETE_TOPIC_MIX_NOTICE'		=> 'Uwaga: Wszystkie tematy, które już są oznaczone jako usunięte nie zostaną naruszone.',
 
 	// Topics - Deleted
-	'PRIME_DELETED_TOPIC_SUCCESS'		=> 'Wybrany temat został pomyślnie oznaczone jako usunięty.',
-	'PRIME_DELETED_TOPICS_SUCCESS'		=> 'Wybrane tematy zostały pomyślnie oznaczone jako usunięte.',
+	'PRIME_DELETED_TOPIC_SUCCESS'		=> 'Wybrany temat został oznaczone jako usunięty.',
+	'PRIME_DELETED_TOPICS_SUCCESS'		=> 'Wybrane tematy zostały oznaczone jako usunięte.',
 	'PRIME_DELETED_TOPIC_FAILURE'		=> 'Wybrany temat nie był oznaczony jako usunięty.',
 	'PRIME_DELETED_TOPICS_FAILURE'		=> 'Wybrane tematy nie były oznaczone jako usunięte.',
 
 	// Topics - Deleted to Trash Bin
-	'PRIME_TRASHED_TOPIC_SUCCESS'		=> 'Wybrany temat został pomyślnie przeniesiony do kosza.',
-	'PRIME_TRASHED_TOPICS_SUCCESS'		=> 'Wybrane tematy zostały pomyślnie przeniesione do kosza.',
+	'PRIME_TRASHED_TOPIC_SUCCESS'		=> 'Wybrany temat został przeniesiony do kosza.',
+	'PRIME_TRASHED_TOPICS_SUCCESS'		=> 'Wybrane tematy zostały przeniesione do kosza.',
 	'PRIME_TRASHED_TOPIC_FAILURE'		=> 'Wybrany temat nie został przeniesiony do kosza.',
 	'PRIME_TRASHED_TOPICS_FAILURE'		=> 'Wybrane tematy nie zostały przeniesione do kosza.',
 	'PRIME_GO_TO_TRASH_BIN'				=> '%sPrzejdź do kosza%s',
@@ -115,39 +115,39 @@ $lang = array_merge($lang, array(
 	'PRIME_UNDELETE_TOPIC_REASON'		=> 'Podaj powód przywrócenia tematu',
 	'PRIME_UNDELETE_TOPIC_CONFIRM'		=> 'Czy na pewno chcesz przywrócić ten temat?',
 	'PRIME_UNDELETE_TOPICS_CONFIRM'		=> 'Czy na pewno chcesz przywrócić te tematy?',
-	'PRIME_UNDELETE_TOPICS_UNNEEDED'	=> 'Wybrane tematy nie moża przywrócić.',
+	'PRIME_UNDELETE_TOPICS_UNNEEDED'	=> 'Wybrane tematy nie mogły zostać przywrócone',
 
 
 	// Topics - Undeleted
-	'PRIME_UNDELETED_TOPIC_SUCCESS'		=> 'Wybrany temat został pomyślnie przywrócony.',
-	'PRIME_UNDELETED_TOPICS_SUCCESS'	=> 'Wybrane tematy zostały pomyślnie przywrócone.',
+	'PRIME_UNDELETED_TOPIC_SUCCESS'		=> 'Wybrany temat został przywrócony.',
+	'PRIME_UNDELETED_TOPICS_SUCCESS'	=> 'Wybrane tematy zostały przywrócone.',
 	'PRIME_UNDELETED_TOPIC_FAILURE'		=> 'Wybrany temat nie został przywrócony.',
 	'PRIME_UNDELETED_TOPICS_FAILURE'	=> 'Wybrane tematy nie zostały przywrócone.',
 
 	// Posts - Deleting
 	'PRIME_DELETE_POST_REASON'			=> 'Podaj powód usunięcia',
-	'PRIME_DELETE_POST_FOREVER'			=> 'Trwale usuń tego posta',
+	'PRIME_DELETE_POST_FOREVER'			=> 'Trwale usuń ten post',
 	'PRIME_DELETE_POSTS_FOREVER'		=> 'Trwale usuń te posty',
 	'PRIME_DELETE_POST_FOREVER_DENIED'	=> 'Nie można trwale usunąć postów na tym forum.',
 	'PRIME_DELETE_POST_MIX_NOTICE'		=> 'Uwaga: Wszystkie posty, które już są oznaczone jako usunięte nie zostaną naruszone.',
 
 	// Posts - Deleted
-	'PRIME_DELETED_POST_SUCCESS'		=> 'Wybrany post został pomyślnie oznaczone jako usunięty.',
-	'PRIME_DELETED_POSTS_SUCCESS'		=> 'Wybrane posty zostały pomyślnie oznaczone jako usunięte.',
+	'PRIME_DELETED_POST_SUCCESS'		=> 'Wybrany post został oznaczone jako usunięty.',
+	'PRIME_DELETED_POSTS_SUCCESS'		=> 'Wybrane posty zostały oznaczone jako usunięte.',
 	'PRIME_DELETED_POST_FAILURE'		=> 'Wybrany post nie został oznaczony jako usunięty.',
 	'PRIME_DELETED_POSTS_FAILURE'		=> 'Wybrane posty nie zostały oznaczone jako usunięte.',
 
 	// Posts - Undeleting
 	'PRIME_UNDELETE_POST'				=> 'Przywróć post',
 	'PRIME_UNDELETE_POSTS'				=> 'Przywróć posty',
-	'PRIME_UNDELETE_POST_REASON'		=> 'Podaj powód przywrócenia posta',
+	'PRIME_UNDELETE_POST_REASON'		=> 'Podaj powód przywrócenia postu',
 	'PRIME_UNDELETE_POST_CONFIRM'		=> 'Czy na pewno chcesz przywrócić ten post?',
 	'PRIME_UNDELETE_POSTS_CONFIRM'		=> 'Czy na pewno chcesz przywrócić te posty?',
-	'PRIME_UNDELETE_POSTS_UNNEEDED'		=> 'Wybrane posty nie mogą być przywrócone.',
+	'PRIME_UNDELETE_POSTS_UNNEEDED'		=> 'Wybrane posty nie mogły zostać przywrócone.',
 
 	// Posts - Undeleted
-	'PRIME_UNDELETED_POST_SUCCESS'		=> 'Wybrany post został pomyślnie przywrócony.',
-	'PRIME_UNDELETED_POSTS_SUCCESS'		=> 'Wybrane posty zostały pomyślnie przywrócone.',
+	'PRIME_UNDELETED_POST_SUCCESS'		=> 'Wybrany post został przywrócony.',
+	'PRIME_UNDELETED_POSTS_SUCCESS'		=> 'Wybrane posty zostały przywrócone.',
 	'PRIME_UNDELETED_POST_FAILURE'		=> 'Wybrany post nie został przywrócony.',
 	'PRIME_UNDELETED_POSTS_FAILURE'		=> 'Wybrane posty nie zostały przywrócone.',
 
