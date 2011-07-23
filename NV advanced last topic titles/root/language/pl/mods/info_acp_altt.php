96873ff4df0920766597fe7dd282d7589858b854
Dariusz Maciejewski <dariuszwm@gmail.com>
2011-07-23 00:15:22 -0700

    Edited NV advanced last topic titles/root/language/pl/mods/info_acp_altt.php via GitHub

--- NV advanced last topic titles/root/language/pl/mods/info_acp_altt.php
+++ NV advanced last topic titles/root/language/pl/mods/info_acp_altt.php
@@ -3,7 +3,7 @@
 *
 * @package - NV Advanced Last Topic Titles
 * @version $Id$
-* @copyright (c) 2007 nickvergessen nickvergessen@gmx.de http://www.flying-bits.org
+* @copyright (c) 2011 daroPL dariuszwm@gmail.com http://www.phpbb.pl
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License 
 *
 */
@@ -18,41 +18,41 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ALTT_ACTIVE'				=> 'enable NV advanced last topic titles MOD',
+	'ALTT_ACTIVE'				=> 'Włącz moda',
 
-	'ALTT_CHAR_LIMIT'			=> 'number of chars, display on the page',
-	'ALTT_CHAR_LIMIT_EXP'		=> '0 or 64 for no cut down',
-	'ALTT_CONFIG'				=> 'configuration of NV advanced last topic titles',
-	'ALTT_CONFIG_SAVED'			=> 'saved adjustment',
+	'ALTT_CHAR_LIMIT'			=> 'Liczba znaków',
+	'ALTT_CHAR_LIMIT_EXP'		=> '0 lub 64, aby wyłączyć obcinanie',
+	'ALTT_CONFIG'				=> 'Konfiguracja moda',
+	'ALTT_CONFIG_SAVED'			=> 'Konfiguracja zapisana',
 
-	'ALTT_LINK_NAME'			=> 'link-name is the title of',
-	'ALTT_LINK_URL'				=> 'link leads to',
-	'ALTT_FIRST_POST'			=> 'First post of last topic',
-	'ALTT_LAST_POST'			=> 'Last post of last topic',
-	'ALTT_FIRST_UNREAD_POST'	=> 'First unread post of last topic',
-	'ALTT_FIRST_UNREAD_POST_NOTE'	=> 'Note: If there is no unread post, the link leads to the first post.',
+	'ALTT_LINK_NAME'			=> 'Tekst odnośnika to tytuł',
+	'ALTT_LINK_URL'				=> 'Odnośnik prowadzi do',
+	'ALTT_FIRST_POST'			=> 'Pierwszego postu ostatniego tematu',
+	'ALTT_LAST_POST'			=> 'Ostatniego postu ostatniego tematu',
+	'ALTT_FIRST_UNREAD_POST'	=> 'Pierwszego nieprzeczytanego postu ostatniego tematu',
+	'ALTT_FIRST_UNREAD_POST_NOTE'	=> 'Uwaga: Jeżeli nie ma nieprzeczytanych postów, odnośnik prowadzi do pierwszego postu.',
 	'ALTT_POST'					=> 'Post',
-	'ALTT_TOPIC'				=> 'Topic',
-	'ALTT_LINK_STYLE'			=> 'Link-Style',
-	'ALTT_BOLD'					=> 'Bold',
-	'ALTT_ITALIC'				=> 'Italic',
-	'ALTT_ADV'					=> 'more:',
+	'ALTT_TOPIC'				=> 'Temat',
+	'ALTT_LINK_STYLE'			=> 'Wygląd odnośnika',
+	'ALTT_BOLD'					=> 'Pogrubiony',
+	'ALTT_ITALIC'				=> 'Kursywa',
+	'ALTT_ADV'					=> 'Więcej:',
 
-	'ALTT_IGNORE_PASSWORD'		=> 'Ignore password',
-	'ALTT_IGNORE_PASSWORD_EXP'	=> 'The title shows up, even if there\'s a forum-password.',
-	'ALTT_IGNORE_RIGHTS'		=> 'Ignore permissions',
-	'ALTT_IGNORE_RIGHTS_EXP'	=> 'If you ignore the permissions, the title shows up, even if you are not able to read the post.',
+	'ALTT_IGNORE_PASSWORD'		=> 'Ignoruj hasło',
+	'ALTT_IGNORE_PASSWORD_EXP'	=> 'Tytuł jest wyświetlany nawet przy forach z hasłem.',
+	'ALTT_IGNORE_RIGHTS'		=> 'Ignoruj uprawnienia',
+	'ALTT_IGNORE_RIGHTS_EXP'	=> 'Tytuł jest wyświetlany nawet jeżeli użytkownik nie ma odpowiednich uprawnień do tematu.',
 
-	'ALTT_PROTECTED'			=> 'This forum is protected.',
+	'ALTT_PROTECTED'			=> 'Forum jest chronione.',
 	'ALTT_TITLE'				=> 'NV advanced last topic titles',
 
-	'NV_ALTT_MOD'					=> '"NV advanced last topic titles" MOD',
-	'INSTALL_NV_ALTT_MOD'			=> 'Install "NV advanced last topic titles" MOD',
-	'INSTALL_NV_ALTT_MOD_CONFIRM'	=> 'Are you sure you want to install the "NV advanced last topic titles" MOD?',
-	'UPDATE_NV_ALTT_MOD'			=> 'Update "NV advanced last topic titles" MOD',
-	'UPDATE_NV_ALTT_MOD_CONFIRM'	=> 'Are you sure you want to update the "NV advanced last topic titles" MOD?',
-	'UNINSTALL_NV_ALTT_MOD'			=> 'Uninstall "NV advanced last topic titles" MOD',
-	'UNINSTALL_NV_ALTT_MOD_CONFIRM'	=> 'Are you sure you want to uninstall the "NV advanced last topic titles" MOD?',
+	'NV_ALTT_MOD'					=> 'Mod NV advanced last topic titles',
+	'INSTALL_NV_ALTT_MOD'			=> 'Zainstaluj moda NV advanced last topic titles',
+	'INSTALL_NV_ALTT_MOD_CONFIRM'	=> 'Czy na pewno chcesz zainstalować moda NV advanced last topic titles?',
+	'UPDATE_NV_ALTT_MOD'			=> 'Aktualizuj moda NV advanced last topic titles',
+	'UPDATE_NV_ALTT_MOD_CONFIRM'	=> 'Czy na pewno chcesz zaktualizować moda NV advanced last topic titles?',
+	'UNINSTALL_NV_ALTT_MOD'			=> 'Odinstaluj moda NV advanced last topic titles',
+	'UNINSTALL_NV_ALTT_MOD_CONFIRM'	=> 'Czy na pewno chcesz odinstalować moda NV advanced last topic titles?',
 ));
 
 ?>
